b3bfbd8f41f9d7a782b59548c880742f90136339
cthanhtuyen290-a11y <cthanhtuyen290@gmail.com>
2026-08-15 13:01:53 +0700

    Update View class with imports and methods
    Added imports and modified View class structure.

--- Assect/view.java
+++ Assect/view.java
@@ -1,1 +1,12 @@
-
+import 3d ("https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js");
+import camera;
+  public class View {
+    public static void View(String[] args)
+  {
+   public String name("$planet");
+     View(this);
+    gl.View.("universe");
+    public String name("$universe");
+  }
+  public class final View {
+  }
